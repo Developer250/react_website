8a72993d863b25b239df308904f0fe8e19161388
Jani Kekalainen <jani.kekalainen@hotmail.com>
2023-10-11 14:33:18 +0200

    Korjaa virheet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
+//COntacting some one
 export const Footer = () => {
     return (
         <footer className="footer">
